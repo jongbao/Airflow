f7afa02259ade0c3b88e0221d0589ba4b403e419
jongbao <whdal1120@gmail.com>
2024-08-22 15:01:32 +0900

    modified: error 수정2

--- dags/dags_bash_with_template.py
+++ dags/dags_bash_with_template.py
@@ -12,14 +12,14 @@ with DAG(
     
     bash_t1 = BashOperator(
         task_id='bash_t1',
-        bash_command='echo "End date is {{ date_interval_end }}"' # 실제 DAG이 돌아가는 시간에 맞춰 나옴
+        bash_command='echo "End date is {{ data_interval_end }}"' # 실제 DAG이 돌아가는 시간에 맞춰 나옴
     )
 
     bash_t2 = BashOperator(
         task_id='bash_t2',
         # 환경변수 설정
-        env={'START_DATE': '{{ date_interval_start | ds }}', # | ds -> yyyy-mm-dd 형태로 출력 가능
-                'END_DATE': '{{ date_interval_end | ds }}'},
+        env={'START_DATE': '{{ data_interval_start | ds }}', # | ds -> yyyy-mm-dd 형태로 출력 가능
+                'END_DATE': '{{ data_interval_end | ds }}'},
         bash_command='echo "Start date is $START_DATE" && ' # && 앞에 있는 command가 성공하면 뒤에 command 실행하겠다
                                 'echo "End date is $END_DATE"'
     )
