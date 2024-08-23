84b9f91fe967e13cbbf71e30d0afc0b52b50f94c
jongbao <whdal1120@gmail.com>
2024-08-23 12:17:55 +0900

    modified: dags_python_template 최종본

--- dags/dags_python_template.py
+++ dags/dags_python_template.py
@@ -20,8 +20,10 @@ with DAG(
     #     print(start_date)
     #     print(end_date)
 
+    # 1번 방식
     # 해당 코드로 작성하면 start_date, end_date와 이름이 맞는 인수를 찾아 바로 할당
-    def python_function1(start_date, end_date):
+    # **kwargs 없어도 동작 가능
+    def python_function1(start_date, end_date, **kwargs):
         print(start_date)
         print(end_date)
     
@@ -32,14 +34,15 @@ with DAG(
     )
 
 
-    # @task(task_id='python_t2')
-    # def python_function2(**kwargs):
-    #     print(**kwargs)
-    #     print('ds:' + kwargs['ds'])
-    #     print('ts:' + kwargs['ts'])
-    #     print('data_interval_start:' + str(kwargs['data_interval_start']))
-    #     print('data_interval_end:' + str(kwargs['data_interval_end']))
-    #     print('task_instance:' + str(kwargs['ti']))
+    # 2번 방식
+    @task(task_id='python_t2')
+    def python_function2(**kwargs):
+        print(**kwargs)
+        print('ds:' + kwargs['ds'])
+        print('ts:' + kwargs['ts'])
+        print('data_interval_start:' + str(kwargs['data_interval_start']))
+        print('data_interval_end:' + str(kwargs['data_interval_end']))
+        print('task_instance:' + str(kwargs['ti']))
 
-    python_t1
-    # python_t1 >> python_function2
+    # task 데코레이터 사용할 때는 함수를 실행하기만 해도 실행가능한 task 생성됨
+    python_t1 >> python_function2()
