39f2d0c2b38639bd47450882a61d5a55b3bc1c98
jongbao <whdal1120@gmail.com>
2024-08-23 10:40:28 +0900

    modified: catchup True로 변경

--- dags/dags_python_show_templates.py
+++ dags/dags_python_show_templates.py
@@ -7,10 +7,10 @@ with DAG(
     dag_id='dags_python_show_templates',
     schedule='30 9 * * *',
     start_date=pendulum.datetime(2024,8,1,tz='Asia/Seoul'),
-    catchup=False
+    catchup=True
 ) as dag:
     
-    @task(task_id='python_task')
+    @task(task_id='python_task') # task operator를 통해 python operator 생성
     def show_templates(**kwargs):
         from pprint import pprint
         pprint(kwargs)
