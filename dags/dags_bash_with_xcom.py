a06061cdbd685efaac9494d6b9b3d7584f1d3d0d
jongbao <whdal1120@gmail.com>
2024-08-27 16:51:19 +0900

    upload: dags_bash_with_xcom.py dag

---
+++ dags/dags_bash_with_xcom.py
@@ -0,0 +1,29 @@
+from airflow import DAG
+import datetime
+import pendulum
+from airflow.operators.bash import BashOperator
+
+with DAG(
+    dag_id="dags_bash_with_xcom",
+    schedule="10 0 * * *",
+    start_date=pendulum.datetime(2024,8,1, tz="Asia/Seoul"),
+    catchup=False
+) as dag:
+    
+    bash_push = BashOperator(
+        task_id='bash_push',
+        bash_command="echo START &&"
+                    "echo XOM_PUSHED"
+                    "{{ ti.xcom_push(key='bash_pushed', value='first_bash_message' )}} &&"
+                    "echo COMPLETE" # 제일 마지막 echo가 return으로 간주 됨
+    )
+
+    bash_pull = BashOperator(
+        task_id='bash_pull',
+        env={'PUSHED_VALUE':"{{ ti.xcom_pull(key='bash_pushed) }}",
+            'RETURN_VALUE':"{{ ti.xcom_pull(task_ids='bash_push') }}"},
+        bash_command="echo $PUSHED_VALUE && echo $RETURN_VALUE",
+        do_xcom_push=False
+    )
+
+    bash_push >> bash_pull
