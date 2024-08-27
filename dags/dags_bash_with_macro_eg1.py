4e667f8dcddf46a4b32a2a922a2ea6105026ccf8
jongbao <whdal1120@gmail.com>
2024-08-27 10:35:16 +0900

    modified: dags_bash_with_macro 예제 dag 에러 수정

--- dags/dags_bash_with_macro_eg1.py
+++ dags/dags_bash_with_macro_eg1.py
@@ -16,5 +16,5 @@ with DAG(
         # 템플릿에서 꺼내 쓰는 날짜 변수들은 타임 존이 기본적으로 utc(한국보다 9시간 느림) 이므로 한국 시간으로 바꿔야 함
         env={'START_DATE':'{{ data_interval_start.in_timezone("Asia/Seoul") | ds }}', # data_interval_strat = 이전 배치에 돌았던 날짜이므로 전월 말일이 됨
              'END_DATE':'{{ (data_interval_end.in_timezone("Asia/Seoul") - macros.dateutil.relativedelta.relativedelta(days=1)) | ds }}'},
-        bash_command='echo "START_DATE: $START_DATE" && "END_DATE: &END_DATE"'
+        bash_command='echo "START_DATE: $START_DATE" && "END_DATE: $END_DATE"'
     )
