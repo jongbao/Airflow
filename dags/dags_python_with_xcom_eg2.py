d1a3ae61b66a25068704c8d6dda7c00cee0af1e8
jongbao <whdal1120@gmail.com>
2024-08-27 16:31:18 +0900

    modified: dags_python_with_xcom_eg2.py 에러 수정

--- dags/dags_python_with_xcom_eg2.py
+++ dags/dags_python_with_xcom_eg2.py
@@ -17,7 +17,7 @@ with DAG(
     
     @task(task_id='python_xcom_pull_1')
     def xcom_pull_1(**kwargs):
-        ti = kwargs['t1']
+        ti = kwargs['ti']
         value1 = ti.xcom_pull(task_ids='python_xcom_push_by_return')
         print('xcom_pull 메소드로 직접 찾은 리턴 값:' + value1)
     
