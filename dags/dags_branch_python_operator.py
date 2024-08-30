89d982fde5722e9de44dfebd662e9e35127a4a9a
jongbao <whdal1120@gmail.com>
2024-08-30 16:48:47 +0900

    modified: dags_branch_python_operator.py error 수정

--- dags/dags_branch_python_operator.py
+++ dags/dags_branch_python_operator.py
@@ -17,7 +17,7 @@ with DAG(
 
         item_list = ['A','B','C']
         selected_item = random.choice(item_list)
-        if selected_item = 'A':
+        if selected_item == 'A':
             return 'task_a'
         elif selected_item in ['B','C']:
             return ['task_b','task_c']
