7eb4ac36362f24c44f007843e15332f896a91316
jongbao <whdal1120@gmail.com>
2024-08-30 16:59:00 +0900

    upload: dags_python_with_branch_decorator.py dag

--- dags/dags_python_with_branch_decorator.py
+++ dags/dags_python_with_branch_decorator.py
@@ -2,30 +2,25 @@ from airflow import DAG
 import datetime
 import pendulum
 from airflow.operators.python import PythonOperator
-from airflow.operators.python import BranchPythonOperator
+from airflow.decorators import task
 import random
 
 with DAG(
-    dag_id="dags_branch_python_operator",
+    dag_id="dags_python_with_branch_operator",
     schedule="30 6 * * *",
     start_date=pendulum.datetime(2024, 8, 1, tz="Asia/Seoul"),
     catchup=False
 ) as dag:
     
+    @task.branch(task_id='python_branch_task')
     def select_random():
         import random
-
         item_list = ['A','B','C']
         selected_item = random.choice(item_list)
         if selected_item == 'A':
             return 'task_a' # 후행 task_id
         elif selected_item in ['B','C']:
             return ['task_b','task_c']
-
-    python_branch_task = BranchPythonOperator(
-        task_id = 'python_branch_task',
-        python_callable=select_random
-    )
 
     def common_func(**kwargs):
         print(kwargs['selected'])
@@ -48,4 +43,5 @@ with DAG(
         op_kwargs={'selected':'C'}
     )
 
-    python_branch_task >> [task_a, task_b, task_c]
+    # 기존에는 파이썬 오퍼레이터를 이용해서 객체를 얻었다면, 해당 내용은 함수를 실행시켜서 객체를 얻음
+    select_random() >> [task_a, task_b, task_c]
