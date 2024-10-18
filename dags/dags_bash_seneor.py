f7052954190bf69b5d5aa7dda6e57f1f31a2c7cb
jongbao <whdal1120@gmail.com>
2024-10-18 15:05:28 +0900

    upload: dags_bash_seneor

---
+++ dags/dags_bash_seneor.py
@@ -0,0 +1,34 @@
+from airflow import DAG
+import pendulum
+from airflow.sensors.bash import BashSensor
+from airflow.operators.bash import BashOperator
+
+with DAG(
+    dag_id='dags_bash_sensor',
+    start_date = pendulum.datetime(2024,10,1, tz='Asia/Seoul'),
+    schedule="0 6 * * *",
+    catchup=False
+) as dag:
+    
+    seneor_task_by_poke = BashSensor(
+        task_id = 'seneor_task_by_poke',
+        env={'FILE':'/opt/airflow/files/tvCorona19VaccineStatNew/{{data_interval_end.in_timezone("Asia/Seoul") | ds_nodash}}/tvCorona19VaccinestatNew.csv'},
+        bash_command=f'''echo $FILE &&
+                        if [ -f $FILE]; then
+                            exit 0
+                        else 
+                            exit 1
+                        fi''',
+        poke_interval=30,
+        timeout=60*2,
+        mode='poke',
+        soft_fail=False
+    )
+
+    bash_task = BashOperator(
+        task_id='bash_task',
+        env={'FILE':'/opt/airflow/files/tvCorona19VaccineStatNew/{{data_interval_end.in_timezone("Asia/Seoul") | ds_nodash}}/tvCorona19VaccinestatNew.csv'},
+        bash_command='echo "건수: `cat $FILE | wc -1`"'
+    )
+
+    [seneor_task_by_poke] >> bash_task
