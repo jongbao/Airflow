671704dfbee8aa7991d8d9a053ef0580c6113535
jongbao <whdal1120@gmail.com>
2024-10-18 15:29:54 +0900

    modified

--- dags/dags_bash_seneor.py
+++ dags/dags_bash_seneor.py
@@ -14,7 +14,7 @@ with DAG(
         task_id = 'seneor_task_by_poke',
         env={'FILE':'/opt/airflow/files/tvCorona19VaccineStatNew/{{data_interval_end.in_timezone("Asia/Seoul") | ds_nodash}}/tvCorona19VaccinestatNew.csv'},
         bash_command=f'''echo $FILE &&
-                        if [ -f $FILE]; then
+                        if [ -f $FILE ]; then
                             exit 0
                         else 
                             exit 1
@@ -29,7 +29,7 @@ with DAG(
         task_id = 'seneor_task_by_reschedule',
         env={'FILE':'/opt/airflow/files/tvCorona19VaccineStatNew/{{data_interval_end.in_timezone("Asia/Seoul") | ds_nodash}}/tvCorona19VaccinestatNew.csv'},
         bash_command=f'''echo $FILE &&
-                        if [ -f $FILE]; then
+                        if [ -f $FILE ]; then
                             exit 0
                         else 
                             exit 1
@@ -43,7 +43,7 @@ with DAG(
     bash_task = BashOperator(
         task_id='bash_task',
         env={'FILE':'/opt/airflow/files/tvCorona19VaccineStatNew/{{data_interval_end.in_timezone("Asia/Seoul") | ds_nodash}}/tvCorona19VaccinestatNew.csv'},
-        bash_command='echo "건수: `cat $FILE | wc -1`"',
+        bash_command='echo "건수: `cat $FILE | wc -l`"',
     )
 
     [seneor_task_by_poke, seneor_task_by_reschedule] >> bash_task
